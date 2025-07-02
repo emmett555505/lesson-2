9cca63e2383df60e68ab0af796dbe6fe6d489c6f
emmett555505 <edonaldson1122@gmail.com>
2025-07-02 16:57:22 -0400

    lesson 2 activities

---
+++ activity1.py
@@ -0,0 +1,14 @@
+#print a sentance
+print('welcome to the world or programing')
+
+#print a number
+print(7)
+
+#next line after print
+print('hello python \n')
+
+#pass multiple arguements for printing
+print('hello',3)
+
+#end arguement of print statment
+print('welcome to',end='*')
---
+++ activity2.py
@@ -0,0 +1,14 @@
+#creating different variable
+x = 5
+y = "john"
+print(x)
+print(y)
+print(x,y)
+
+codingal = "codingal"
+print(codingal)
+
+#user imput
+name = input("enter your name: ")
+
+print("\nhello", name , "\nwelcome to codingal")
